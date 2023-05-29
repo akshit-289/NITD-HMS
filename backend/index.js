ab73e00f4070a441b161e3d3c29344648367c29b
Akshit Aggarwal <aggarwal.akshit289@gmail.com>
2023-05-30 00:42:02 +0530

    backend URL changed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoDb = require("./db");
 mongoDb();
 app.use((req, res, next)=>{
